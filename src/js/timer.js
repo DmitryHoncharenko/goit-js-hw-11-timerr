72f1d15b51eac2d608213c7e4a73d6aa6cf26cba
DmitryHoncharenko <67203474+DmitryHoncharenko@users.noreply.github.com>
2021-01-11 20:47:21 +0200

    fixCode

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,8 +1,8 @@
-import refs from "../refs/timerRefs.js";
+import refs from '../refs/timerRefs.js';
 
 class CountdownTimer {
   constructor(selector, targetDate) {
-    this.$el = document.querySelector(selector);
+    this.selector = document.querySelector(selector);
     this.targetDate = targetDate;
   }
   updateClockItems(time) {
@@ -24,19 +24,40 @@ class CountdownTimer {
   pad(value) {
     return String(value).padStart(2, '0');
   }
-  loadingTimer = setInterval(() => {
+
+  start() {
     const startTime = Date.now();
     let diff = this.targetDate - startTime;
     this.updateClockItems(diff);
-  }, refs.sec);
+    setInterval(() => {
+      const startTime = Date.now();
+      let diff = this.targetDate - startTime;
+      this.updateClockItems(diff);
+    }, 1000);
+  }
 }
 
-new CountdownTimer(
+const timer = new CountdownTimer(
   ...Object.values({
     selector: '#timer-1',
     targetDate: new Date('Jul 17, 2021'),
   }),
 );
 
+timer.start();
+//   pad(value) {
+//     return String(value).padStart(2, '0');
+//   }
 
+//   loadingTimer = setInterval(() => {
+//     const startTime = Date.now();
+//     let diff = this.targetDate - startTime;
+//     this.updateClockItems(diff);
+//   }, refs.sec);
 
+// new CountdownTimer(
+//   ...Object.values({
+//     selector: '#timer-1',
+//     targetDate: new Date('Jul 17, 2021'),
+//   }),
+// );
